5dc1db9eebcb0382101c45b7103eb6e03049eb22
Endy Muhardin <endy.muhardin@gmail.com>
2012-12-20 17:43:35 +0700

    rabbitmq sebagai channel

---
+++ src/main/java/com/muhardin/endy/belajar/spring/integration/RabbitChannel.java
@@ -0,0 +1,14 @@
+package com.muhardin.endy.belajar.spring.integration;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class RabbitChannel {
+	public static void main(String[] args) {
+		System.out.println("Silahkan mengetik pesan");
+		
+		AbstractApplicationContext ctx 
+		= new ClassPathXmlApplicationContext("classpath:rabbit-channel.xml");
+		ctx.registerShutdownHook();
+	}
+}
